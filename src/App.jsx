9764e9debc5a43497f70d90af7d96672e8b2a5a7
CodeSandbox <support@codesandbox.io>
2023-11-30 09:15:07 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Player from "./Components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1 " symbol="x" />
+          <Player name="player 2" symbol="0" />
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
