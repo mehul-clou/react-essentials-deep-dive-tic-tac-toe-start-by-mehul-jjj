a7a8a5b78fd0316866d7ecde213d15e8565260e1
mj <85698909+mehul-clou@users.noreply.github.com>
2023-12-05 10:22:54 +0000

    Update 2 files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GameBoard from "./Components/GameBoard";
 import Log from "./Components/Log";
 import { useState } from "react";
 import { WINNING_COMBINATIONS } from "./winning-combination";
+import GameOver from "./Components/GameOver";
 
 const initialGameBoard = [
   [null, null, null],
@@ -52,6 +53,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     //setActvePlayer((curActivePlayer) => (curActivePlayer === "X" ? "0" : "X"));
 
@@ -74,7 +77,8 @@ function App() {
           <Player name="Player 1 " symbol="X" isActive={activePlayer === "X"} />
           <Player name="player 2" symbol="0" isActive={activePlayer === "0"} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelected={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
